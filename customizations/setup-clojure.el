ac85d5e0594c5f0ae031302f25f652ac40dbd10d
Joseph Watkins <joseph.o.watkins@gmail.com>
2015-04-17 10:00:17 +0100

    Add company-mode

--- customizations/setup-clojure.el
+++ customizations/setup-clojure.el
@@ -48,6 +48,10 @@
 ;; enable paredit in your REPL
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
+;; enable company-mode for cider (JW)
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+
 ;; Use clojure mode for other extensions
 (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.boot$" . clojure-mode))
