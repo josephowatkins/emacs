f3676cdfec4abf996d2b3cf19ed95610aa12f49d
Joseph Watkins <joseph.o.watkins@gmail.com>
2017-10-24 21:21:37 +0100

    WIP configure helm for emacs

---
+++ customizations/helm-setup.el
@@ -0,0 +1,16 @@
+;; helm setup
+(require 'helm-config)
+
+; (global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") #'helm-find-files)
+
+(helm-mode 1)
+
+;; (setq helm-projectile-fuzzy-match nil)
+(require 'helm-projectile)
+(helm-projectile-on)
+
+(require 'helm-smex)
+(global-set-key [remap execute-extended-command] #'helm-smex)
+(global-set-key (kbd "M-X") #'helm-smex-major-mode-commands)
